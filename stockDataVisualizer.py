aad5fa5d06830c0b974b91c72d6c809f2cbc862e
Lzobz <zobelluke@gmail.com>
2024-03-13 15:07:57 -0500

    Added code to get dates to python code

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -1,1 +1,16 @@
+year1=int(input("Enter a start year(Ex: 2002):"))
+month1=int(input("Enter a start month(Ex: 08):"))
+day1=int(input("Enter a start day(Ex: 13):"))
 
+year2=int(input("Enter a end year(Ex: 2002):"))
+month2=int(input("Enter a end month(Ex: 08):"))
+day2=int(input("Enter a end day(Ex: 13):"))
+
+if year1>year2:
+    print("invalid date (The begining date year is after the end date year)")
+elif year1==year2 and month1>month2:
+    print("invalid date (The begining date month is after the end date month)")
+elif  year1==year2 and month1>month2 and day1>day2:
+    print("invalid date (The begining date day is after the end date day)")
+else:
+    print("Correct dates!") 
