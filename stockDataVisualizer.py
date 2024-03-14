4bc6a06aedc35e1ff8697c706f4705bbe625fe74
Logan Cockrell <loganmcockrell@gmail.com>
2024-03-13 19:28:12 -0500

    added basic API requests
    intradaily, daily, weekly and monthly. only passes the symbol the user entered because it seems the API does not accept dates

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -1,1 +1,19 @@
+import requests
 
+def intradaily(symbol):
+    return 'https://alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + symbol + '&interval=5min&apikey=K7HLGROEFZW2C06M'
+
+def daily(symbol):
+    return 'https://alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=full&apikey=K7HLGROEFZW2C06M'
+
+def weekly(symbol):
+    return 'https://alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=' + symbol + '&outputsize=full&apikey=K7HLGROEFZW2C06M'
+
+def monthly(symbol):
+    return 'https://alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=' + symbol + '&outputsize=full&apikey=K7HLGROEFZW2C06M'
+
+url = daily('IBM')
+r = requests.get(url)
+data = r.json()
+
+print(data)
