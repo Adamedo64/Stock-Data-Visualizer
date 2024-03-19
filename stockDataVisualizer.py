38145002c7d0966ba1d208b3f26712c0aeeacc25
Lzobz <zobelluke@gmail.com>
2024-03-19 14:02:41 -0500

    updated data to change date type to string

--- stockDataVisualizer.py
+++ stockDataVisualizer.py
@@ -1,16 +1,17 @@
-year1=int(input("Enter a start year(Ex: 2002):"))
-month1=int(input("Enter a start month(Ex: 08):"))
-day1=int(input("Enter a start day(Ex: 13):"))
+while True:
+    start_date = input("Enter the start date (YYYY-MM-DD): ")
+    end_date = input("Enter the end date (YYYY-MM-DD): ")
 
-year2=int(input("Enter a end year(Ex: 2002):"))
-month2=int(input("Enter a end month(Ex: 08):"))
-day2=int(input("Enter a end day(Ex: 13):"))
+    try:
+        start_y, start_m, start_d = start_date.split('-')
+        end_y, end_m, end_d = end_date.split('-')
 
-if year1>year2:
-    print("invalid date (The begining date year is after the end date year)")
-elif year1==year2 and month1>month2:
-    print("invalid date (The begining date month is after the end date month)")
-elif  year1==year2 and month1>month2 and day1>day2:
-    print("invalid date (The begining date day is after the end date day)")
-else:
-    print("Correct dates!") 
+        if (start_y, start_m, start_d) > (end_y, end_m, end_d):
+            print("Start date cannot be later than end date. Please try again.")
+        else:
+            break
+    except ValueError:
+        print("Invalid date format. Please enter date in YYYY-MM-DD format.")
+
+print("Start date:", start_date)
+print("End date:", end_date)
